d09b20975b210d74c844467a752e227de8f93f88
7Collector <148943338+7Collector@users.noreply.github.com>
2024-03-02 11:13:37 +0530

    Created some Activities
    Created Splash Activity for checking login state and added login activity will follow up on that soon
    created setup activity plans are setting up some APIs and storing the results and maintaining a schedule list saved on the device that would be updated on the timespan of 7 or more days Just activities have been created and flow has been decided research on APIs is going through

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database:20.3.1")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
     implementation("com.google.firebase:firebase-firestore")
